ff615935918904ae9b1e5ea25d850d9497e211b5
Leo Qian <rq48@cornell.edu>
2025-02-13 21:04:51 -0500

    first draft of improvePolicy function

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,17 @@ import java.util.HashMap;
  */
 public class Main {
     /** {@code vπ(s)}: Given a state s, returns the discounted stateToReward of that state assuming {@link #policy} is followed */
-    private static HashMap<Integer,Double> stateValueFunction;
+    private static HashMap<Integer, Double> stateValueFunction;
 
     /** {@code qπ(s,a)}: Given a state s and an action a, returns the discounted stateToReward of the next states assuming {@link #policy} is followed */
-    private static final HashMap<intPairs,Double> actionValueFunction;
+    private static final HashMap<intPairs, Double> actionValueFunction;
 
     /** {@code π(s,a)}: Given a state s and an action a, returns the probability that this action is taken in state s */
-    private static final HashMap<intPairs,Double> policy;
+    private static final HashMap<intPairs, Double> policy;
 
     /** {@code r}: In this instance, the stateToReward is associated purely to a state, however in most
      * cases it is associated with a state-action pair (s,a) or a state transition (s,a,s') */
-    private static final HashMap<Integer,Double> stateToReward;
+    private static final HashMap<Integer, Double> stateToReward;
 
     /** The probability that the imaginary coin flip lands on head and the gambler wins his gambled money */
     private static final double probWinGamble = 0.4;
@@ -29,48 +29,69 @@ public class Main {
         actionValueFunction = new HashMap<>();
         stateToReward = new HashMap<>();
 
-        stateValueFunction.put(0,0.0);
-        for(int state = 1; state <100; state++){
-            stateValueFunction.put(state,0.0);
-            for(int action = 1; action <Math.min(state,100-state); action++) {
+        stateValueFunction.put(0, 0.0);
+        for (int state = 1; state < 100; state++) {
+            stateValueFunction.put(state, 0.0);
+            for (int action = 1; action <= Math.min(state, 100 - state); action++) {
                 actionValueFunction.put(new intPairs(state, action), 0.0);
-                policy.put(new intPairs(state, action),1.0/ state);
+                policy.put(new intPairs(state, action), 1.0 / state);
             }
         }
-        stateValueFunction.put(100,0.0);
+        stateValueFunction.put(100, 0.0);
     }
 
     public static void main(String[] args) {
 
     }
 
-    /** Creates a new policy that is deterministic and greedy with respect to the state value function */
-    public static void improvePolicy(){
-        //todo implement
+    /** Creates a new policy that is deterministic and greedy with respect to the action value function */
+    public static void improvePolicy() {
+        for (int state = 1; state < 100; state++) {
+            for (int action = 1; action <= Math.min(state, 100 - state); action++) {
+                policy.replace(new intPairs(state, action), (action == getBestActionFromState(state) ? 1.0 : 0.0));
+            }
+        }
+    }
+
+    public static int getBestActionFromState(int state) {
+        int maxAction = 0;
+        double maxActionValue = Double.MIN_VALUE;
+        for (int action = 1; action <= Math.min(state, 100 - state); action++) {
+            double actionValue = actionValueFunction.get(new intPairs(state, action));
+            if (actionValue > maxActionValue) {
+                maxAction = action;
+                maxActionValue = actionValue;
+            }
+        }
+        return maxAction;
     }
 
     /** Reiterates and reassign the state-value function according to the bellman's equation */
-    public static void updateStateValueFunction(){
-        HashMap<Integer,Double> newStateValueFunction = new HashMap<>();
-        for(int i=0;i<=100;i++){
-            newStateValueFunction.put(i,updateStateValue(i));
+    public static void updateStateValueFunction() {
+        HashMap<Integer, Double> newStateValueFunction = new HashMap<>();
+        for (int i = 0; i <= 100; i++) {
+            newStateValueFunction.put(i, updateStateValue(i));
         }
         stateValueFunction = newStateValueFunction;
     }
 
-    public static double updateStateValue(int state){
+    /** Returns the updated state-value function's output at this specific state.<br>
+     * Also updates the output of action-value function of all outgoing actions from this state */
+    public static double updateStateValue(int state) {
         double newStateValue = 0;
-        for(int action = 1; action <= Math.min(state,100-state); action++){
+        for (int action = 1; action <= Math.min(state, 100 - state); action++) {
             //the gambler bets (action) amount of money, possible states are either win or lose that money
             double nextStateValueSum = 0;
-            for (int nextState : new int[]{state+action,state-action}){
+            for (int nextState : new int[]{state + action, state - action}) {
                 //stateToReward is defined by the nextState
                 double reward = stateToReward.get(nextState);
                 //p(s',r|s,a) = (winning ? probWinGamble : 1 - probWinGamble)
-                nextStateValueSum += (nextState > state ? probWinGamble : 1 - probWinGamble) * (reward + discountRate * stateValueFunction.get(nextState));
+                double newActionValue = (nextState > state ? probWinGamble : 1 - probWinGamble) * (reward + discountRate * stateValueFunction.get(nextState));
+                actionValueFunction.replace(new intPairs(state, action), newActionValue);
+                nextStateValueSum += newActionValue;
             }
 
-            newStateValue += policy.get(new intPairs(state,action)) * nextStateValueSum;
+            newStateValue += policy.get(new intPairs(state, action)) * nextStateValueSum;
         }
 
         return newStateValue;
